595ab1f38f8c31e7e3e22abd6194be6b97e083a1
Lahari Divya <laharidivyad@gmail.com>
2026-08-20 09:15:05 +0530

    Add files via upload

---
+++ Task11.java
@@ -0,0 +1,15 @@
+class Task11 {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        // Move fast pointer 2 steps and slow pointer 1 step at a time
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        // When fast reaches the end, slow is at the middle node
+        return slow;
+    }
+}
