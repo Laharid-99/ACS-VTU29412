c748f146939ec42f74d8f426e4e34b4deafa5264
Lahari Divya <laharidivyad@gmail.com>
2026-08-24 09:13:08 +0530

    Add files via upload

---
+++ Task12.java
@@ -0,0 +1,31 @@
+class Task12 {
+ public boolean isPalindrome(ListNode head) {
+ ListNode slow = head;
+ ListNode fast = head.next;
+
+ while (fast != null && fast.next != null) {
+ slow = slow.next;
+ fast = fast.next.next;
+ }
+
+  ListNode current = slow.next;
+ slow.next = null; // Disconnect the first half from the second half
+
+
+ ListNode previous = null;
+ while (current != null) {
+ ListNode temp = current.next; 
+ current.next = previous; 
+ previous = current;
+ current = temp;  }
+
+ while (previous != null) {
+ if (previous.val != head.val) {
+ return false;  }
+ previous = previous.next;
+ head = head.next;
+ }
+
+ return true;
+  }
+}
