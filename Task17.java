6923b5fbd610852c5f6b9880a95ec08c8a000ba8
Lahari Divya <laharidivyad@gmail.com>
2026-08-27 09:28:46 +0530

    Add files via upload

---
+++ Task17.java
@@ -0,0 +1,19 @@
+public class Task17 {         
+    public ListNode detectCycle(ListNode head) {                 
+        ListNode fastPointer = head;         
+        ListNode slowPointer = head;                        
+        while (fastPointer != null && fastPointer.next != null) {             
+            slowPointer = slowPointer.next;                     
+            fastPointer = fastPointer.next.next;                  
+            if (slowPointer == fastPointer) {                                                   
+                ListNode startPointer = head;                                              
+                     while (startPointer != slowPointer) {                    
+                     startPointer = startPointer.next;                     
+                    slowPointer = slowPointer.next;                 
+                }                                               
+                return startPointer;            
+            }         
+        }                        
+        return null;     
+    } 
+}
